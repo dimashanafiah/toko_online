fe1a4ec5dd05c76678c9983eb4dfb5b03ac0c6b7
dimashan <dimas.han19@gmail.com>
2018-11-18 10:36:21 +0700

    install mongoose dan create koneksi database

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require ("express");
 var path = require ('path');
-
+var mongoose = require ('mongoose');
 //initial app
 var app = express();
+
+//connection syntax
+mongoose.connect('mongodb://localhost/toko-online');
+var db = mongoose.connection;
+db.on ('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+    console.log("sudah connect ke mongodb");
+});
 
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -12,7 +20,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get("/", function(rec,res){
-    res.send('ini adalah index');
+    res.send('ini adalah index dan sudah konek ke mongodb');
 });
 //setup server
 var port = 3000;
